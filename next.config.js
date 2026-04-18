2d272de0bb978d47464c3edccbd7f7b1cb42e787
Vishnu V Ram <vishnuvramforpc@gmail.com>
2026-04-18 23:51:09 +0530

    error fix

---
+++ next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  // This is a Next.js config stub for Vercel detection
+  // Actual build is handled by vercel.json buildCommand
+  webpack: (config) => {
+    return config;
+  },
+};
